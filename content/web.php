7c2eb0b6122c81ac9727ba4e53b2e1418b0d38e0
Aaron Weidele <aweidele@r2integrated.com>
2017-03-24 14:23:49 -0400

    slider animation

--- content/web.php
+++ content/web.php
@@ -1,5 +1,10 @@
 <section id="<?php echo $a; ?>">
   <div class="content-fluid">
-Web
+    <div class="col-4 offset-1">
+      here.
+    </div>
+    <div class="col-5 offset-1">
+      <img src="<?php echo $root_url.'templates/'.$theme.'/images/ipad.svg'; ?>">
+    </div>
   </div>
 </section>
