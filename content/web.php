d23dc779e995fffa2446677e03fc309c409094ed
Aaron Weidele <aweidele@r2integrated.com>
2017-03-24 16:28:49 -0400

    skills styling

--- content/web.php
+++ content/web.php
@@ -1,7 +1,23 @@
 <section id="<?php echo $a; ?>">
   <div class="content-fluid">
     <div class="col-4 offset-1">
-      here.
+      <h2>Front-End Web Development</h2>
+      <h3>Related Skills:</h3>
+      <ul class="skills">
+        <li>Adobe Creative Suite (Photoshop, Illustrator, InDesign)</li>
+        <li>HTML 5</li>
+        <li>CSS 3</li>
+        <li>PHP</li>
+        <li>Javascript/JQuery</li>
+        <li>Wordpress</li>
+        <li>Sitecore</li>
+        <li>Shopify</li>
+        <li>MySQL</li>
+        <li>PayPal/Payflow</li>
+        <li>Web Prototyping tools (Axure, Balsamic, HotGloo)</li>
+        <li>UX Design</li>
+        <li>Usability Testing</li>
+      </ul>
     </div>
     <div class="col-5 offset-1">
       <div class="web-preview">
