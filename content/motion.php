b227360e4508c325e0a01886bc2599f21acebfb1
Aaron Weidele <aweidele@r2integrated.com>
2017-03-30 15:40:15 -0400

    Add Motion section

--- content/motion.php
+++ content/motion.php
@@ -1,5 +1,28 @@
 <section id="<?php echo $a; ?>">
   <div class="content-fluid">
-Motion
+    <div class="col-5 offset-1">
+      <div class="motion-preview">
+        <img src="<?php echo $root_url.'templates/'.$theme.'/images/tv.svg'; ?>">
+      </div>
+    </div>
+    <div class="col-4 offset-1">
+      <h2>Motion</h2>
+      <h3>Related Skills:</h3>
+      <ul class="skills">
+        <li>Adobe Creative Suite (Photoshop, Illustrator, InDesign)</li>
+        <li>HTML 5</li>
+        <li>CSS 3</li>
+        <li>PHP</li>
+        <li>Javascript/JQuery</li>
+        <li>Wordpress</li>
+        <li>Sitecore</li>
+        <li>Shopify</li>
+        <li>MySQL</li>
+        <li>PayPal/Payflow</li>
+        <li>Web Prototyping tools (Axure, Balsamic, HotGloo)</li>
+        <li>UX Design</li>
+        <li>Usability Testing</li>
+      </ul>
+    </div>
   </div>
 </section>
