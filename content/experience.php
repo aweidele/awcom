bc93db311f12ea102efaeb9d24175634807fc900
aweidele <aweidele@gmail.com>
2017-05-06 14:10:17 -0400

    Style the work experience section

--- content/experience.php
+++ content/experience.php
@@ -1,5 +1,35 @@
+<style type="text/css">
+<?php
+$today = intval(date('Y'));
+$years = array();
+foreach ($jobs as $key => $job) {
+	$years[] = $job['start'];
+}
+$years[] = $today;
+$range = $years[sizeof($years) -1] - $years[0];
+$i = 1;
+$p = 0;
+foreach($jobs as $key => $job) { 
+	$w = ($years[$i] - $job['start']) / $range * 100;
+?>
+	#<?php echo $key; ?> {
+		width: <?php echo $w; ?>%;
+		left: <?php echo $p; ?>%;
+	}
+<?php 
+	$p += $w;
+	$i++;
+} ?>
+</style>
 <section id="<?php echo $a; ?>">
   <div class="content-fluid">
-<pre><?php print_r($jobs); ?></pre>
+  	<ul class="work-timeline">
+<?php foreach($jobs as $key => $job) { ?>
+	<li id="<?php echo $key; ?>">
+		<h3><?php echo $job['company']; ?></h3>
+		<p><?php echo $job['position']; ?></p>
+	</li>
+<?php } ?>
+	</ul>
   </div>
 </section>
--- content/portfolio.php
+++ content/portfolio.php
@@ -16,7 +16,7 @@
     'colossus' => array(
       'name' => 'DuClaw Brewing Company: Colossus',
       'description' => 'Front-end development for brewery promotional site',
-      'url' => 'http://dorfestival.com/',
+      'url' => 'http://angrychickens.com/-subdomains/hosted.aaronweidele.com/colossus/',
       'image' => 'col.jpg',
     ),
     'ck' => array(
