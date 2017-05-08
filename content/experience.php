4c45c43d1b26429209192a89e0ee464e952057a2
Aaron Weidele <aweidele@r2integrated.com>
2017-05-08 09:55:10 -0400

    restyling the timeline

--- content/experience.php
+++ content/experience.php
@@ -1,26 +1,3 @@
-<style type="text/css">
-<?php
-$today = intval(date('Y'));
-$years = array();
-foreach ($jobs as $key => $job) {
-	$years[] = $job['start'];
-}
-$years[] = $today;
-$range = $years[sizeof($years) -1] - $years[0];
-$i = 1;
-$p = 0;
-foreach($jobs as $key => $job) { 
-	$w = ($years[$i] - $job['start']) / $range * 100;
-?>
-	#<?php echo $key; ?> {
-		width: <?php echo $w; ?>%;
-		left: <?php echo $p; ?>%;
-	}
-<?php 
-	$p += $w;
-	$i++;
-} ?>
-</style>
 <section id="<?php echo $a; ?>">
   <div class="content-fluid">
   	<h2>Work Experience</h2>
