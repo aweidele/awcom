1735e3f37fb2571fb08bd67161567a890a61b8bc
R2INTEGRATED\aweidele <aweidele@r2integrated.com>
2018-10-18 11:26:38 -0400

    Remove files from parent branch

--- content/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $theme = '2017-theme';
-  $root_url = 'http://www.aaronweidele.com/';
-  $root_uri = '/home1/angrychi/public_html/-subdomains/aaronweidele.com';
-  $page_title = "Aaron Weidele";
-?>
