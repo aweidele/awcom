bf64b9f9de886abdb0e4f8d5f58072f279c23ddc
Aaron Weidele <aweidele@r2integrated.com>
2019-09-04 15:55:22 -0400

    Styling the portfolio

--- docroot/js/scripts.js
+++ docroot/js/scripts.js
@@ -13639,7 +13639,27 @@ return jQuery;
 
 (function($) {
   $(document).ready(function() {
-    console.log("Hello");
-    $('.owl-carousel').owlCarousel();
+    owlOn = false;
+    owlOn = windowResize(owlOn);
+    $(window).on('resize',function() {
+      owlOn = windowResize(owlOn);
+    });
   });
 })(jQuery);
+
+function windowResize(owlOn) {
+  var w = $(window).width();
+  //$('#feedback').text(w);
+  if(w < 768 && !owlOn) {
+    $('.owl-carousel').owlCarousel({
+      items: 1,
+      loop: true
+    });
+    owlOn = true;
+  } else if(w >= 768 && owlOn) {
+    $('.owl-carousel').owlCarousel('destroy');
+    owlOn = false;
+  }
+
+  return owlOn;
+}
--- src/js/01_script.js
+++ src/js/01_script.js
@@ -1,6 +1,26 @@
 (function($) {
   $(document).ready(function() {
-    console.log("Hello");
-    $('.owl-carousel').owlCarousel();
+    owlOn = false;
+    owlOn = windowResize(owlOn);
+    $(window).on('resize',function() {
+      owlOn = windowResize(owlOn);
+    });
   });
 })(jQuery);
+
+function windowResize(owlOn) {
+  var w = $(window).width();
+  //$('#feedback').text(w);
+  if(w < 768 && !owlOn) {
+    $('.owl-carousel').owlCarousel({
+      items: 1,
+      loop: true
+    });
+    owlOn = true;
+  } else if(w >= 768 && owlOn) {
+    $('.owl-carousel').owlCarousel('destroy');
+    owlOn = false;
+  }
+
+  return owlOn;
+}
