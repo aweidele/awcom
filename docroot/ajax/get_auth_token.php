006097a5301089dc8af6f4954441e98db50b5429
Aaron Weidele <aweidele@gmail.com>
2019-09-16 21:26:38 -0400

    oops

--- docroot/ajax/get_auth_token.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$redirectUri = 'http://aaronweidele.com/ajax/phpmailer/get_oauth_token.php';
-$clientId = '1035018324404-6ia281gkh3ushni1h741s2cb1bhctcam.apps.googleusercontent.com';
-$clientSecret = '6YI42zgmgLfGueDV1TtpEDyk';
