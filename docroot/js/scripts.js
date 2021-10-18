04f378849c667f954ac73082ac1d2487fe88b4de
Aaron Weidele <aweidele@gmail.com>
2021-10-18 13:33:39 -0400

    Smooth scrolling

--- docroot/js/scripts.js
+++ docroot/js/scripts.js
@@ -1,5 +1,7 @@
 (() => {
   const body = document.querySelector('body');
+
+  // Menu Toggle
   const menuToggle = document.querySelector('.header_inner button');
   let menuOpen = false;
 
@@ -14,4 +16,17 @@
       menuOpen = true;
     }
   });
+
+  // Navigation Scroll
+  const jumpLink = document.querySelectorAll('a[href^="#"]');
+  jumpLink.forEach((el)=>{
+    el.addEventListener('click',function(e) {
+      e.preventDefault();
+
+      document.querySelector(e.target.closest('a').getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+
+      body.classList.remove('menu_open');
+      body.classList.add('menu_closed');
+    });
+  })
 })();
