91e34d7a63eaeac4fdceca8056b3e7d1a1073246
Aaron Weidele <>
2024-10-22 15:48:36 -0400

    skills icons

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -12,8 +12,10 @@ export default function SkillCard({ skill }) {
       <div class="relative max-w-42 max-h-42 aspect-square mx-auto">
         <DonutGraph className="size-full my-0 relative z-10" percent={skill.level / 100} />
         {imgSrc && (
-          <figure className="w-full h-full absolute top-0 left-0 rounded-full overflow-hidden flex items-center justify-center">
-            <img src={imgSrc} onError={handleImageError} className="size-logo object-contain" />
+          <figure className="size-full absolute top-0 left-0 flex items-center justify-center">
+            <div class="size-logo rounded-full overflow-hidden">
+              <img src={imgSrc} onError={handleImageError} className="size-full object-contain" />
+            </div>
           </figure>
         )}
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,7 @@ export default {
       },
       spacing: {
         42: pxToRem(60),
-        logo: "65%",
+        logo: "65.5%",
       },
       keyframes: {
         menuOpenTop: {
