267c9af8d2862f986bd2f307bf1f941993ac349e
AARON WEIDELE <aaronweidele@mac-mini.myfiosgateway.com>
2024-12-05 14:35:58 -0500

    Updating some content

--- src/content/sections/portfolio.js
+++ src/content/sections/portfolio.js
@@ -2,7 +2,7 @@ const works = [
   {
     id: "walters",
     title: "The Walters Art Museum",
-    description: "Custom Wordpress Theme for a Baltimore Art Museum",
+    description: "Developed a responsive, visually engaging WordPress theme that showcases the Walters Art Museum's rich collections, exhibitions, and cultural significance while driving visitor engagement and museum support.",
     url: "https://thewalters.org/",
     skills: ["html", "css", "javascript", "wordpress"],
     images: ["walters.jpg"],
@@ -12,7 +12,7 @@ const works = [
   {
     id: "umaf",
     title: "Accelerate—The Campaign for UMass Amherst",
-    description: `Drupal theme and Storybook for campaign site.`,
+    description: `Developed a custom Drupal theme for the UMass Amherst campaign site; Created a comprehensive component library using Storybook and TailwindCSS, which integrated seamlessly into the theme, establishing a modular design system.`,
     url: "https://accelerateumass.org/",
     skills: ["html", "css", "javascript", "drupal", "tailwind", "storybook"],
     images: ["umass.jpg"],
@@ -20,9 +20,19 @@ const works = [
     mobile: [],
   },
   {
+    id: "prescott",
+    title: "Prescott College",
+    description: `Developed a custom WordPress theme for Prescott College that enhances digital content management and user experience. Created custom Gutenberg blocks and block patterns to provide an intuitive authoring experience, while implementing key features such as a detailed faculty directory, comprehensive list of degree programs`,
+    url: "https://prescott.edu/",
+    skills: ["wordpress", "html", "css", "javascript"],
+    images: ["prescott.jpg"],
+    video: "prescott.mp4",
+    mobile: [],
+  },
+  {
     id: "meadville",
     title: "Meadville Lombard Theological School",
-    description: `Front End Development for higher education`,
+    description: `Front-end development for Meadville Lombard Theological School using BigtreeCMS as the content management backend. Implemented sophisticated interactive elements and navigated challenging design requirements`,
     url: "https://www.meadville.edu/",
     skills: ["html", "css", "javascript"],
     images: ["meadville.jpg"],
@@ -31,4 +41,4 @@ const works = [
   },
 ];
 
-export const portfolio = ["walters", "umaf", "meadville"].map((item) => works.filter((work) => work.id === item)[0]);
+export const portfolio = ["walters", "umaf", "prescott", "meadville"].map((item) => works.filter((work) => work.id === item)[0]);
