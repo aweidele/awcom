42cf932120c673556ee75f184a41640cf2d2e483
Aaron Weidele <aweidele@gmail.com>
2025-04-08 06:24:01 -0400

    Setting up base routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RootLayout } from "./pages/RootLayout";
+import { Homepage } from "./pages/Homepage";
+import { Blog } from "./pages/Blog";
+import { Projects } from "./pages/Projects";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <div>Error.</div>,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <p>hey</p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/RootLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+export const RootLayout = () => {
+  return (
+    <>
+      <header>Header</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
+    </>
+  );
+};
