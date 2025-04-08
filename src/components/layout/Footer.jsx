589fe1f54cba3c9d56edbfe397e8d08d043c5438
Aaron Weidele <aweidele@gmail.com>
2025-04-08 16:39:40 -0400

    Footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,42 @@
+import { Section } from "./Section";
+import Heading from "../elements/Heading";
+import Grid from "./Grid";
+import { LinkedInLogo, BlueSkyLogo, GitHubLogo } from "../elements/Icons";
+
+export const Footer = () => {
+  return (
+    <Section tag="footer" className="bg-s2 text-white py-32">
+      <Heading l={3}>Connect With Me</Heading>
+      <Grid>
+        <div className="col-span-5">Mail Form Here</div>
+        <div className="col-span-7 max-md:pt-10">
+          <ul className="flex justify-center md:justify-end gap-2">
+            <li>
+              <a href="https://www.linkedin.com/in/aaron-weidele/" target="_blank">
+                <LinkedInLogo className="h-8 w-8 fill-current" />
+                <span className="sr-only">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/aweidele.bsky.social" target="_blank">
+                <BlueSkyLogo className="h-8 w-8 fill-current" />
+                <span className="sr-only">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/aweidele" target="_blank">
+                <GitHubLogo className="h-8 w-8 fill-current" />
+                <span className="sr-only">GitHub</span>
+              </a>
+            </li>
+          </ul>
+          <p className="text-xs text-center md:text-right font-sans my-4">
+            <a href="https://github.com/aweidele/awcom/tree/sites/2024b-site" target="_blank" className="underline hover:text-p2 duration-300">
+              See the repo for this site!
+            </a>
+          </p>
+        </div>
+      </Grid>
+    </Section>
+  );
+};
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Footer } from "../components/layout/Footer";
 export const RootLayout = () => {
   return (
     <>
+      <Footer />
       <header>Header</header>
       <main>
         <Outlet />
       </main>
-      <footer>Footer</footer>
     </>
   );
 };
