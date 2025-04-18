0ccf7b98d8fd44fc4d3836bdd0338ea98118682f
Aaron Weidele <aweidele@gmail.com>
2025-04-17 23:25:40 -0400

    styling blog post

--- src/components/elements/ProjectPost.jsx
+++ src/components/elements/ProjectPost.jsx
@@ -5,9 +5,21 @@ export const ProjectPost = ({ content, skills, acf }) => {
   return (
     <Section className="blog-post py-16">
       <Grid>
-        <div className="col-span-2">Sidebar</div>
+        <div className="col-span-2">
+          <h3 className="font-semibold">Relevant skills</h3>
+          <ul className="mb-4 max-sm:columns-2 max-md:columns-3">
+            {skills.map((skill) => (
+              <li key={skill.slug} className="text-xs flex gap-2 items-center">
+                <div className="w-6 h-6 rounded-full overflow-hidden border border-s2-lt2">
+                  <img src={skill.icon} alt={`${skill.name} Icon`} />
+                </div>
+                <span>{skill.name}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
         <div className="col-span-8">
-          <div dangerouslySetInnerHTML={{ __html: content.rendered }} />
+          <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: content.rendered }} />
         </div>
       </Grid>
     </Section>
