4389795439821b6d32cc110f795de2090bb6fb18
Aaron Weidele <aweidele@gmail.com>
2025-04-21 11:05:22 -0400

    Adding project link

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,7 +1,9 @@
-export function Button({ children, target, tag = "a", newWindow = false, extraClasses }) {
+export function Button({ children, target, tag = "a", newWindow = false, extraClasses, color = "p2", size = "reg" }) {
   const Tag = tag;
+  const sizeClass = size && size === "sm" ? "px-2 py-1 md:px-4" : "px-4 py-3 md:px-8";
+  const bgClass = color && color === "p" ? "bg-s hover:bg-s2 text-white" : "bg-p2 hover:bg-p2-dk text-black";
   return (
-    <Tag href={target} target={newWindow ? "_blank" : null} className={`bg-p2 hover:bg-p2-dk text-black font-sans text-xs md:text-reg uppercase inline-block px-4 py-3 md:px-8 mt-5 duration-300 max-md:w-full max-md:text-center ${extraClasses}`}>
+    <Tag href={target} target={newWindow ? "_blank" : null} className={`btn ${bgClass} font-sans text-xs md:text-reg uppercase inline-block ${sizeClass} mt-5 duration-300 max-md:w-full max-md:text-center ${extraClasses}`}>
       {children}
     </Tag>
   );
--- src/components/elements/ProjectPost.jsx
+++ src/components/elements/ProjectPost.jsx
@@ -1,11 +1,16 @@
+import { Button } from "./Button";
 import Grid from "../layout/Grid";
 import { Section } from "../layout/Section";
 
 export const ProjectPost = ({ content, skills, acf }) => {
+  console.log(acf);
   return (
     <Section className="blog-post py-16">
       <Grid>
         <div className="col-span-2">
+          <Button target={acf.project_link} extraClasses="mt-0 mb-4 text-white" size="sm" color="p">
+            Launch Project
+          </Button>
           <h3 className="font-semibold">Relevant skills</h3>
           <ul className="mb-4 max-sm:columns-2 max-md:columns-3">
             {skills.map((skill) => (
