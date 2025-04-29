5fbfb46fdcbe0aa383ba317062778d09cb31ea76
Aaron Weidele <aweidele@gmail.com>
2025-04-29 14:14:28 -0400

    padding

--- src/components/layout/Section.jsx
+++ src/components/layout/Section.jsx
@@ -1,8 +1,13 @@
-export const Section = ({ className, children, tag = "section", fullwidth = false, id }) => {
+export const Section = ({ className, children, tag = "section", fullwidth = false, id, padding = "lg" }) => {
   const Tag = tag;
+  const paddingClasses = {
+    sm: "px-4",
+    md: "px-8",
+    lg: "px-16",
+  };
   return (
     <Tag id={id} className={className}>
-      {fullwidth ? <>{children}</> : <div className="max-w-c px-16 desktopXl:px-5 m-auto">{children}</div>}
+      {fullwidth ? <>{children}</> : <div className={`max-w-c ${paddingClasses[padding]} desktopXl:px-5 m-auto`}>{children}</div>}
     </Tag>
   );
 };
