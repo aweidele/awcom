61ebb10d0685143b7a1c824d38df347752aee5bd
Aaron Weidele <aweidele@gmail.com>
2026-08-01 11:39:59 -0400

    Building out components

---
+++ src/components/Layout/Header.stories.tsx
@@ -0,0 +1,8 @@
+import HeaderAstroComponent from "./Header.astro";
+
+export default {
+  title: 'Layout/Header',
+  component: HeaderAstroComponent,
+};
+
+export const Header = {};
---
+++ src/components/Layout/Logo.stories.tsx
@@ -0,0 +1,8 @@
+import LogoAstroComponent from "./Logo.astro";
+
+export default {
+  title: 'Layout/Header/Elements',
+  component: LogoAstroComponent,
+};
+
+export const Logo = {};
