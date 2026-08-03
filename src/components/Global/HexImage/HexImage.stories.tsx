48a92d4e7b8606c43395bcd717581831d81a3957
Aaron Weidele <aweidele@gmail.com>
2026-08-02 22:00:09 -0400

    Hex image

--- src/components/Global/HexImage/HexImage.stories.tsx
+++ src/components/Global/HexImage/HexImage.stories.tsx
@@ -1,8 +1,31 @@
 import HexImageAstroComponent from "./HexImage.astro";
+import * as Logos from "../../../logos";
 
 export default {
   title: 'Global/Hex Image',
-  component: HexImageAstroComponent
+  component: HexImageAstroComponent,
+  argTypes: {
+    width: {
+      control: "number",
+      name: "Width"
+    },
+    image: {
+      control: "select",
+      name: "Image",
+      options: Object.keys(Logos),
+      mapping: Logos
+    },
+    strokeColor: {
+      control: "color",
+      name: "Stroke Color"
+    }
+  }
 }
 
-export const HexImage = {};
+export const HexImage = {
+  args: {
+    width: 115,
+    image: "javascriptLogo",
+    strokeColor: "#ae9e32"
+  }
+};
