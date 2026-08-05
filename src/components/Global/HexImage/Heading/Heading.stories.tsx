5c7dd4e67266ddc977a226702836d0022a2de8cb
Aaron Weidele <aaron.weidele@aristotle.com>
2026-08-05 15:08:17 -0400

    Headings stories

--- src/components/Global/HexImage/Heading/Heading.stories.tsx
+++ src/components/Global/HexImage/Heading/Heading.stories.tsx
@@ -1,17 +1,50 @@
 import HeadingAstroComponent, { type Props as HeadingProps } from "./Heading.astro";
 
 export default {
-  title: 'Global/Typography/Heading',
+  title: 'Global/Typography/Headings',
   component: HeadingAstroComponent,
   argTypes: {
     level: {
       control: { type: "number", min: 1, max: 6, step: 1 },
+    },
+    className:{
+      control: { type: "text" },
     }
   }
 }
 
-export const Heading = {
+export const Heading_1 = {
+  args: {
+    level: 1,
+  } satisfies HeadingProps
+};
+
+export const Heading_2 = {
   args: {
     level: 2
+  } satisfies HeadingProps
+};
+
+export const Heading_3 = {
+  args: {
+    level: 3
+  } satisfies HeadingProps
+};
+
+export const Heading_4 = {
+  args: {
+    level: 4
+  } satisfies HeadingProps
+};
+
+export const Heading_5= {
+  args: {
+    level: 5
+  } satisfies HeadingProps
+};
+
+export const Heading_6 = {
+  args: {
+    level: 6
   } satisfies HeadingProps
 };
